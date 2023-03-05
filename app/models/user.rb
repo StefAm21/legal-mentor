9f6965282b19134c74820f4a3f1f4b1dfa2c6a48
stefano amodei <samodei@udesa.edu.ar>
2023-03-05 19:48:28 -0300

    reacomodando questions/index con conexion a la base de datos

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :reviews, dependent: :destroy
+  has_many :questions
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_one_attached :photo
