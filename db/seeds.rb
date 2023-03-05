afa69707bf5f383d89de2d4e7edcbac3d3ff3731
stefano amodei <samodei@udesa.edu.ar>
2023-03-05 20:50:21 -0300

    filling the seed with some samples

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,16 +22,69 @@ Question.create!(
   user: common_user
 )
 
-# common_user.questions.create!(
-#   title: 'How can I do correctly my divorce papers?',
-#   description: 'I am ending my marriage................... sdbasjfbadshcfbdaskcckhdsabckjdbckjdsbckjsdbcjdkcbsjkdbc',
-#   category: " ",
-#   status: " "
+Question.create!(
+  title: 'How can I organize my taxes?',
+  description: 'I am building my start up................... sdbasjfbadshcfbdaskcckhdsabckjdbckjdsbckjsdbcjdkcbsjkdbc',
+  category: "Tax",
+  user: common_user
+)
+
+Question.create!(
+  title: 'How can I organize my taxes?',
+  description: 'I am building my start up................... sdbasjfbadshcfbdaskcckhdsabckjdbckjdsbckjsdbcjdkcbsjkdbc',
+  category: "Tax",
+  user: common_user
+)
+
+Question.create!(
+  title: 'How can I organize my taxes?',
+  description: 'I am building my start up................... sdbasjfbadshcfbdaskcckhdsabckjdbckjdsbckjsdbcjdkcbsjkdbc',
+  category: "Tax",
+  user: common_user
+)
+
+puts "creating new user"
+# Create a new USER
+new_user = User.create!(
+  email: 'new_user@example.com',
+  password: 'password'
+)
+
+puts "creating questions for new_user"
+# Create questions for new_user
+Question.create!(
+  title: 'What are some good marketing strategies for a small business?',
+  description: 'I recently started a small business and I am looking for some effective marketing strategies.',
+  category: "Marketing",
+  user: new_user
+)
+
+Question.create!(
+  title: 'What is the best way to raise funding for a tech startup?',
+  description: 'I have a great idea for a tech startup, but I need funding to make it a reality. What are some effective ways to raise funds?',
+  category: "Startup",
+  user: new_user
+)
+
+Question.create!(
+  title: 'What are the top skills required for a successful career in software development?',
+  description: 'I am interested in pursuing a career in software development. What are the key skills I should focus on developing?',
+  category: "Software Development",
+  user: new_user
+)
+
+
+
+# Create a new LAWYER
+# lawyer = User.create!(
+#   email: 'layer@example.com',
+#   password: 'password'
 # )
 
-# common_user.questions.create!(
-#   title: 'How can I do a succession of lands?',
-#   description: 'I am inhariting some lands................... sdbasjfbadshcfbdaskcckhdsabckjdbckjdsbckjsdbcjdkcbsjkdbc',
-#   category: " ",
-#   status: " "
+# Question.create!(
+#   question:
+#   # price: 100,
+#   comment: 'I am interested in anwering this question I am more than qualified.... ajsjdbdhbcshbckhsb',
+#   avg_time: 2,
+#   user: layer,
 # )
