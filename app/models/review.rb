645689bf8819fdeb6769a4737bd3049ef902b5ea
stefano amodei <samodei@udesa.edu.ar>
2023-03-06 13:59:31 -0300

    adding some backend to the reviews logic with a helper

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :user
   # Validates
-  validates :rating, :comment, presence: true
+  validates :rating, numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 5 }
+  validates :comment, presence: true
 end
