7d56968bfc96b0d2d1370cc177a237699d62f63c
Daniel Godoy <godoydaniel97@gmail.com>
2023-03-07 13:03:30 -0300

    Questions almost ready

--- app/models/question.rb
+++ app/models/question.rb
@@ -5,5 +5,31 @@ class Question < ApplicationRecord
   # Validates
   validates :title, :description, :category, :status, :user, presence: true
 
+  include AASM
 
+  aasm do
+    state :waiting, initial: true
+    state :choosing, :paid, :answered, :ended, :discarted
+
+    event :new_answer do
+      transitions from: :waiting, to: :choosing
+    end
+
+    event :choose do
+      transitions from: :choosing, to: :paid
+    end
+
+    event :answer_delivered do
+      transitions from: :paid, to: :answered
+    end
+
+    event :finalize do
+      transitions from: :answered, to: :ended
+    end
+
+    event :discard do
+      transtions from: [:waiting, :choosing], to: :discarted
+    end
+  end
+  # waiting, con_respuestas, pagada, respondida, terminada
 end
