db164927addabb7c59145de5de6d6fe1277c2a2c
Daniel Godoy <godoydaniel97@gmail.com>
2023-03-08 22:40:02 -0300

    add more seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -75,16 +75,64 @@ Question.create!(
 
 
 
+puts "creating new lawyer"
 # Create a new LAWYER
-# lawyer = User.create!(
-#   email: 'layer@example.com',
-#   password: 'password'
-# )
+lawyer = User.create!(
+  email: 'lawyer@example.com',
+  password: 'password'
+)
 
-# Question.create!(
-#   question:
-#   # price: 100,
-#   comment: 'I am interested in anwering this question I am more than qualified.... ajsjdbdhbcshbckhsb',
-#   avg_time: 2,
-#   user: layer,
-# )
+puts "creating answers for common_user"
+# Create answers for common_user's questions
+common_user.questions.each do |question|
+  Answer.create!(
+    question: question,
+    price: 100,
+    comment: 'I am interested in answering this question. I am more than qualified to provide a thorough response.',
+    avg_time: 2,
+    user: lawyer,
+  )
+end
+
+puts "creating answers for new_user"
+# Create answers for new_user's questions
+new_user.questions.each do |question|
+  Answer.create!(
+    question: question,
+    price: 150,
+    comment: 'As a seasoned professional in this field, I can offer valuable insights and strategies for success.',
+    avg_time: 3,
+    user: lawyer,
+  )
+end
+puts "creating another lawyer"
+# Create another LAWYER
+another_lawyer = User.create!(
+  email: 'another_lawyer@example.com',
+  password: 'password'
+)
+
+puts "creating answers for common_user by another_lawyer"
+# Create answers for common_user's questions by another_lawyer
+common_user.questions.each do |question|
+  Answer.create!(
+    question: question,
+    price: 120,
+    comment: 'I am a specialist in this area and can provide detailed and actionable advice for your situation.',
+    avg_time: 2.5,
+    user: another_lawyer,
+  )
+end
+
+puts "creating answers for new_user by another_lawyer"
+# Create answers for new_user's questions by another_lawyer
+new_user.questions.each do |question|
+  Answer.create!(
+    question: question,
+    price: 180,
+    comment: 'I have helped numerous clients in similar situations and can offer personalized strategies to achieve your goals.',
+    avg_time: 3.5,
+    user: another_lawyer,
+  )
+end
+
