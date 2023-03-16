61e7dbb3e84b105ec2ebc81501d94a3fd7878c77
Daniel Godoy <godoydaniel97@gmail.com>
2023-03-16 14:10:27 -0300

    Fixing the url

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: %i[name last_name lawyer])
   end
 
-  def after_sign_in_path_for(resource)  # rubocop:disable Lint/UnusedMethodArgument
+  def after_sign_in_path_for(resource_or_scope)  # rubocop:disable Lint/UnusedMethodArgument
+    "https://www.legalmentor.me"
     if current_user.lawyer
       answers_path
     else
