75295b1e0c8f667dd393e9a6ac12051b17c9879a
Daniel Godoy <godoydaniel97@gmail.com>
2023-03-17 14:23:26 -0300

    fixing MP

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_15_221601) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_17_172129) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -87,7 +87,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_15_221601) do
   create_table "payments", force: :cascade do |t|
     t.string "status"
     t.string "status_detail"
-    t.integer "mp_id"
+    t.string "mp_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
